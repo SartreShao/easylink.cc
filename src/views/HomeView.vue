99f8a041a314b8e4a8e6a016075e2758c85f2dad
邵励治 <shaolizhi1234@126.com>
2022-09-24 01:30:42 +0800

    add background img

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <template>
+  <img class="background" src="../assets/background.png" />
 </template>
 
 <script setup>
 import Store from "@/store";
 import { inject, watchEffect } from "vue";
-
-
 </script>
+
+<style scoped lang="scss">
+.background {
+  position: fixed;
+  object-fit: cover;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+  width: 100%;
+  pointer-events: none;
+}
+</style>
