0aff0279614c3a512d8cf108b5eb611fbabd6517
邵励治 <shaolizhi1234@126.com>
2022-10-06 10:14:37 +0800

    init leancloud

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import AV from "leancloud-storage";
+
+AV.init({
+  appId: "Eg39w11lqO8cv6GPphyxCQRF-gzGzoHsz",
+  appKey: "6es1crbDK0vqy7NQNelNmBuI",
+  serverURL: "https://ishaolizhi.api.hearfresh.cn",
+});
 
 const app = createApp(App);
 
